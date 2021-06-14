e1ee322ded79e82a6c6b325d21091b04096d1b14
dazacher <dazacher2000@yahoo.com>
2021-06-14 09:47:20 -0700

    Property binding to the Allow Server button.

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,12 +6,17 @@ import { Component, OnInit } from '@angular/core';
   //selector: '[app-servers]',
   //making the selector a class
   //selector: '.app-servers',
-  template: '<app-server></app-server><app-server></app-server>',
+  //template: '<app-server></app-server><app-server></app-server>',
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-
-  constructor() { }
+  allowNewServer = false;
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000)
+   }
 
   ngOnInit(): void {
   }
