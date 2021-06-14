665ac494e2f9d232057d84c9705fc04964ad80a8
dazacher <dazacher2000@yahoo.com>
2021-06-14 12:03:16 -0700

    Class activity 2, add an input with two-binding, a paragraph with string interpolation.

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -14,6 +14,7 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!'
   serverName = 'Test Server';
+  userName = '';
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
@@ -24,7 +25,7 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer(){
-    this.serverCreationStatus = 'Server was created!'
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
   }
 
   onUpdateServerName(event: any){
