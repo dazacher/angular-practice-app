e285a91a4db6a898948e10ecfbc9c277b4b3aa81
dazacher <dazacher2000@yahoo.com>
2021-06-14 09:35:47 -0700

    String Interpolation.

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
+  //making it an attribute instead of a selector
+  //selector: '[app-servers]',
+  //making the selector a class
+  //selector: '.app-servers',
   template: '<app-server></app-server><app-server></app-server>',
   styleUrls: ['./servers.component.css']
 })
